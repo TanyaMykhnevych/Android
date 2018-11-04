ee28d8323678bae6e060ee866a6c7fb1f7847a51
TanyaMy <tatyana.mykhnevych@gmail.com>
2018-11-04 10:21:21 +0200

    Added validation not to save empty note

--- Labs/Lab1/NotesApp/app/src/main/java/ua/nure/notesapp/activities/NotePreviewActivity.java
+++ Labs/Lab1/NotesApp/app/src/main/java/ua/nure/notesapp/activities/NotePreviewActivity.java
@@ -3,6 +3,7 @@ package ua.nure.notesapp.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -37,27 +38,28 @@ public class NotePreviewActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent();
                 Note noteToSave = getNoteToSave();
-                intent.putExtra("saved_note", noteToSave);
-                setResult(RESULT_OK, intent);
-                finish();
+                if (noteToSave != null) {
+                    intent.putExtra("saved_note", noteToSave);
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
             }
         });
     }
 
     private Note getNoteToSave() {
         Note note = noteToEdit == null ? new Note() : noteToEdit;
-        note.setTitle(((EditText) findViewById(R.id.NoteTitle)).getText().toString());
-        note.setDescription(((EditText) findViewById(R.id.NoteDescription)).getText().toString());
 
-        String importance = ((Spinner) findViewById(R.id.NoteImportance)).getSelectedItem().toString();
-
-        if (importance == getResources().getString(R.string.importance_high)) {
-            note.setImportance(Importance.HIGH);
-        } else if (importance == getResources().getString(R.string.importance_low)) {
-            note.setImportance(Importance.LOW);
-        } else {
-            note.setImportance(Importance.NORMAL);
+        if(!setTitle((note))){
+            return null;
         }
+
+        if(!setDescription((note))){
+            return null;
+        }
+
+        setImportance(note);
+
         return note;
     }
 
@@ -65,5 +67,38 @@ public class NotePreviewActivity extends AppCompatActivity {
         ((EditText) findViewById(R.id.NoteTitle)).setText(note.getTitle());
         ((EditText) findViewById(R.id.NoteDescription)).setText(note.getDescription());
         ((Spinner) findViewById(R.id.NoteImportance)).setSelection(note.getImportance().getValue());
+    }
+
+    private boolean setTitle(Note note){
+        EditText title = ((EditText) findViewById(R.id.NoteTitle));
+        if( TextUtils.isEmpty(title.getText())){
+            title.setError( getResources().getString(R.string.title_required_error));
+            return false;
+
+        }
+        note.setTitle(title.getText().toString());
+        return true;
+    }
+
+    private boolean setDescription(Note note){
+        EditText description = ((EditText) findViewById(R.id.NoteDescription));
+        if( TextUtils.isEmpty(description.getText())){
+            description.setError( getResources().getString(R.string.description_required_error));
+            return false;
+
+        }
+        note.setDescription(description.getText().toString());
+        return true;
+    }
+
+    private void setImportance(Note note){
+        String importance = ((Spinner) findViewById(R.id.NoteImportance)).getSelectedItem().toString();
+        if (importance == getResources().getString(R.string.importance_high)) {
+            note.setImportance(Importance.HIGH);
+        } else if (importance == getResources().getString(R.string.importance_low)) {
+            note.setImportance(Importance.LOW);
+        } else {
+            note.setImportance(Importance.NORMAL);
+        }
     }
 }
